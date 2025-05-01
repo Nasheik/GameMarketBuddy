37521cb3a02a0c6feb552cf166dc5afe5e2733b1
Kishan <kshnjshptl@gmail.com>
2025-04-30 23:52:49 -0400

    added observe

--- redis/src/index.js
+++ redis/src/index.js
@@ -1,5 +1,10 @@
 export default {
     async scheduled(controller, env, ctx) {
+        ctx.waitUntil(processJobs(env));
         console.log("cron processed");
     },
 };
+
+async function processJobs(env) {
+    console.log("cron processed");
+}
